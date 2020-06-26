54cc5c50ae80e4f28966190c301655d334db3f3e
Ariel Parra <darkaico@gmail.com>
2020-06-26 10:58:42 +0200

    simplifying app

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import os
-
 from api import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', debug=True)
