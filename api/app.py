e2730bace59582978539e7863348c0b8ad5d96b0
Ariel Parra <darkaico@gmail.com>
2020-06-26 10:45:59 +0200

    Better routes

--- api/app.py
+++ api/app.py
@@ -1,139 +1,16 @@
 import os
 
-from flask import (
-    Flask,
-    jsonify,
-    request
-)
+from flask import Flask
 
-from mini_redis import (
-    CommandError,
-    MiniRedis
-)
+from api.routes import api_routes
 
 app = Flask(__name__)
-
-
-def create_error_response(msg):
-    return jsonify({
-        'error': msg
-    })
-
-
-def create_success_response(msg=''):
-    return jsonify({
-        'message': msg
-    })
+app.register_blueprint(api_routes)
 
 
 @app.route('/')
 def index():
-    return 'Hello, Android!'
-
-
-@app.route(r'/api/store/<key>', methods=['GET'])
-def get_key(key):
-    try:
-        response = MiniRedis.instance().get(key)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return create_success_response({
-        'value': response
-    })
-
-
-@app.route(r'/api/store/<key>', methods=['PUT'])
-def set_key(key):
-    try:
-        data = request.json
-    except ValueError:
-        return create_error_response('not a valid request data')
-
-    value = data.get('value')
-
-    try:
-        response = MiniRedis.instance().set(key, value)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return create_success_response(response)
-
-
-@app.route(r'/api/store/<key>', methods=['DELETE'])
-def delete_key(key):
-    response = MiniRedis.instance().delete(key)
-
-    return create_success_response(response)
-
-
-@app.route(r'/api/store/<key>/incr', methods=['PUT'])
-def put_incr(key):
-    try:
-        response = MiniRedis.instance().incr(key)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return create_success_response(response)
-
-
-@app.route(r'/api/store/<key>/zadd', methods=['PUT'])
-def put_zadd(key):
-    try:
-        data = request.json
-    except ValueError:
-        return create_error_response('not a valid request data')
-
-    score = data.get('score')
-    member = data.get('member')
-
-    try:
-        response = MiniRedis.instance().zadd(key, score, member)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return jsonify({
-        "value": response
-    })
-
-
-@app.route(r'/api/store/<key>/zcard')
-def get_zcard(key, method=['GET']):
-
-    try:
-        response = MiniRedis.instance().zcard(key)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return jsonify({
-        'value': response
-    })
-
-
-@app.route(r'/api/store/<key>/zrank/<member>', methods=['GET'])
-def get_zrank(key, member):
-
-    response = MiniRedis.instance().zrank(key, member)
-
-    return jsonify({
-        'value': response
-    })
-
-
-@app.route(r'/api/store/<key>/zrange', methods=['GET'])
-def get_zrange(key):
-
-    start = request.args.get('start')
-    stop = request.args.get('stop')
-
-    try:
-        response = MiniRedis.instance().zrange(key, start, stop)
-    except CommandError as e:
-        return create_error_response(e)
-
-    return jsonify({
-        'range': response
-    })
+    return 'Hello, Avocado! 🥑'
 
 
 if __name__ == '__main__':
--- api/routes.py
+++ api/routes.py
@@ -1,148 +1,129 @@
-import json
-
-from aiohttp import web
+from flask import (
+    Blueprint,
+    jsonify,
+    request
+)
 
 from mini_redis import (
     CommandError,
     MiniRedis
 )
 
-routes = web.RouteTableDef()
+api_routes = Blueprint('api', __name__)
 
 
-def create_success(response):
-    """Create simple response
-
-    :param response: dict/str content
-    :return 200 with response content
-    """
-    return web.Response(text=json.dumps(response))
+def create_error_response(msg):
+    return jsonify({
+        'error': msg
+    })
 
 
-def create_server_error(error_response: Exception):
-    """Create simple server error
-
-    :param error_response: Exception instance
-    :return 500 error with message
-    """
-    return web.Response(text=str(error_response), status=500)
+def create_success_response(msg=''):
+    return jsonify({
+        'message': msg
+    })
 
 
-def create_request_error(error_response: CommandError):
-    """Create simple command error
-
-    :param error_response: CommandError instance
-    :return 400 error with message
-    """
-    return web.Response(text=str(error_response), status=400)
-
-
-@routes.get(r'/api/dbsize')
-async def get_db_size(request):
-    try:
-        dbsize = MiniRedis.instance().dbsize()
-    except CommandError as e:
-        return create_request_error(e)
-
-    return web.Response(text=json.dumps(dbsize))
-
-
-@routes.get(r'/api/store/{key}')
-async def get_key(request):
-    key = request.match_info['key']
-
+@api_routes.route(r'/api/store/<key>', methods=['GET'])
+def get_key(key):
     try:
         response = MiniRedis.instance().get(key)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return create_success_response({
+        'value': response
+    })
 
 
-@routes.put(r'/api/store/{key}')
-async def set_key(request):
-    key = request.match_info['key']
+@api_routes.route(r'/api/store/<key>', methods=['PUT'])
+def set_key(key):
     try:
-        data = await request.json()
+        data = request.json
     except ValueError:
-        return create_request_error('not a valid request data')
+        return create_error_response('not a valid request data')
 
     value = data.get('value')
 
     try:
         response = MiniRedis.instance().set(key, value)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return create_success_response(response)
 
 
-@routes.delete(r'/api/store/{key}')
-async def delete_key(request):
-    key = request.match_info['key']
+@api_routes.route(r'/api/store/<key>', methods=['DELETE'])
+def delete_key(key):
     response = MiniRedis.instance().delete(key)
 
-    return create_success(response)
+    return create_success_response(response)
 
 
-@routes.put(r'/api/store/{key}/incr')
-async def put_incr(request):
-    key = request.match_info['key']
-
+@api_routes.route(r'/api/store/<key>/incr', methods=['PUT'])
+def put_incr(key):
     try:
         response = MiniRedis.instance().incr(key)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return create_success_response(response)
 
 
-@routes.put(r'/api/store/{key}/zadd')
-async def put_zadd(request):
-    key = request.match_info['key']
-    score = request.query.get('score')
-    member = request.query.get('member')
+@api_routes.route(r'/api/store/<key>/zadd', methods=['PUT'])
+def put_zadd(key):
+    try:
+        data = request.json
+    except ValueError:
+        return create_error_response('not a valid request data')
+
+    score = data.get('score')
+    member = data.get('member')
 
     try:
         response = MiniRedis.instance().zadd(key, score, member)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return jsonify({
+        "value": response
+    })
 
 
-@routes.get(r'/api/store/{key}/zcard')
-async def get_zcard(request):
-    key = request.match_info['key']
+@api_routes.route(r'/api/store/<key>/zcard')
+def get_zcard(key, method=['GET']):
 
     try:
         response = MiniRedis.instance().zcard(key)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return jsonify({
+        'value': response
+    })
 
 
-@routes.get(r'/api/store/{key}/zrank/{member}')
-async def get_zrank(request):
-    key = request.match_info['key']
-    member = request.match_info['member']
+@api_routes.route(r'/api/store/<key>/zrank/<member>', methods=['GET'])
+def get_zrank(key, member):
 
     response = MiniRedis.instance().zrank(key, member)
 
-    return create_success(response)
+    return jsonify({
+        'value': response
+    })
 
 
-@routes.get(r'/api/store/{key}/zrange')
-async def get_zrange(request):
+@api_routes.route(r'/api/store/<key>/zrange', methods=['GET'])
+def get_zrange(key):
 
-    key = request.match_info['key']
-    start = request.query.get('start')
-    stop = request.query.get('stop')
+    start = request.args.get('start')
+    stop = request.args.get('stop')
 
     try:
         response = MiniRedis.instance().zrange(key, start, stop)
     except CommandError as e:
-        return create_request_error(e)
+        return create_error_response(e)
 
-    return create_success(response)
+    return jsonify({
+        'range': response
+    })
